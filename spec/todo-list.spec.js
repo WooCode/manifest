e94a9f94d43b39151d416e0d71cc9054f10366c8
ahrentorp_woocode <jonas@woocode.com>
2020-03-12 11:09:03 +0100

    Add tests

--- spec/todo-list.spec.js
+++ spec/todo-list.spec.js
@@ -7,3 +7,11 @@ test("Does nothing when just regular text.", () => {
 test("Creates todo list of one item.", () => {
   expect(compileTodoList("- [ ] foo")).toBe('<ul><li><input disabled="" type="checkbox"> foo</li></ul>');
 });
+
+test("Creates todo list of one checked item, lower case x.", () => {
+  expect(compileTodoList("- [x] foo")).toBe('<ul><li><input disabled="" checked="" type="checkbox"> foo</li></ul>');
+});
+
+test("Creates todo list of one checked item, upper case x.", () => {
+  expect(compileTodoList("- [X] foo")).toBe('<ul><li><input disabled="" checked="" type="checkbox"> foo</li></ul>');
+});
